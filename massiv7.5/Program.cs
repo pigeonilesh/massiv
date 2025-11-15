c8267e273e23cb43ebd041469fc0d70dd7af6359
pigeonIlesh <ilyanikulin0820@gmail.com>
2025-11-15 18:00:28 +0300

    Начал делать

---
+++ massiv7.5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace massiv7._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[][] mas = new string[6][];
+            mas[0] = new string[] {"Иванов Иван Васильевич", "Директор"};
+            mas[1] = new string[] {"Петров Генадий Генадьевич", "Секретарь"};
+            mas[2] = new string[] {"Степанов Степан Степанович", "Менеджер продаж"};
+            mas[3] = new string[] {"Григорьев Григорий Григорьевич", "Менеджер рекламы"};
+            mas[4] = new string[] {"Александров Александр Александрович", "Бухгалтер"};
+            mas[5] = new string[] {"Гулкин Миша Иванович", "Рабочий"};
+            foreach (string[] s1 in mas)  
+            {
+                foreach (string s2 in s1)
+                {
+                    Console.WriteLine($"{s2} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Введите должность сотрудников которых хотите найти:");
+            string a = Console.ReadLine();
+            foreach (string[] s1 in mas)
+            {
+                if (s1[1].ToLower() == a.ToLower())
+                {
+                    Console.WriteLine($"{s1[0]}");
+                }
+            }
+
+        }
+    }
+}
