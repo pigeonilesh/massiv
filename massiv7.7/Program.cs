03a8c4af2b0a0b1bc8a5ce890d5a479eba74bf52
pigeonIlesh <ilyanikulin0820@gmail.com>
2025-11-17 09:55:32 +0300

    сделал 7

---
+++ massiv7.7/Program.cs
@@ -0,0 +1,70 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace massiv7._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[][] mas = new string[10][];
+            mas[0] = new string[] {"Банан", "20", "Продукты"};
+            mas[1] = new string[] {"Клавиатура", "1500", "Техника"};
+            mas[2] = new string[] {"Парацетамол", "500", "Лекарства"};
+            mas[3] = new string[] {"Наушники", "600", "Техника"};
+            mas[4] = new string[] {"Нурафен", "700", "Лекарства"};
+            mas[5] = new string[] {"Яблоко", "15", "Продукты"};
+            mas[6] = new string[] {"Апельсин", "40", "Продукты"};
+            mas[7] = new string[] {"Ручка", "70", "Канцелярия"};
+            mas[8] = new string[] {"Степлер", "250", "Канцелярия"};
+            mas[9] = new string[] {"Киви", "100", "Продукты"};
+            foreach (string[] tovar in mas) 
+            {
+                Console.WriteLine($"{tovar[0]} - {tovar[1]} руб. ({tovar[2]})");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Введите категорию товаров, которые хотите найти:");
+            string kat = Console.ReadLine();
+            foreach (string[] tovar in mas)
+            {
+                if (tovar[2].ToLower() == kat.ToLower())
+                {
+                    Console.WriteLine($"{tovar[0]} - {tovar[1]} руб. ({tovar[2]})");
+                }
+            }
+            Console.WriteLine();
+            Array.Sort(mas, (x, y) => string.Compare(x[2], y[2]));
+            Console.WriteLine("Отсортированный список по категориям:");
+            foreach (string[] tovar in mas)
+            {
+                Console.WriteLine($"{tovar[0]} - {tovar[1]} руб. ({tovar[2]})");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Введите ценовой диапозон для поиска товаров.");
+            Console.WriteLine($"Введите минимум:");
+            int min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Введите максимум:");
+            int max = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine($"Товары в диапазоне от {min} до {max} руб.:");
+            foreach (string[] tovar in mas)
+            {
+                int price = Convert.ToInt32(tovar[1]);
+                if (price >= min && price <= max)
+                {
+                    Console.WriteLine($"{tovar[0]} - {tovar[1]} руб. ({tovar[2]})");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Отсортированный список по цене:");
+            Array.Sort(mas, (x, y) => { int pricex = Convert.ToInt32(x[1]), pricey = Convert.ToInt32(y[1]); return pricex.CompareTo(pricey); });
+            foreach (string[] tovar in mas)
+            {
+                Console.WriteLine($"{tovar[0]} - {tovar[1]} руб. ({tovar[2]})");
+            }
+        }
+    }
+}
---
+++ massiv7.7/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("massiv7.7")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("massiv7.7")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("7ef6ddee-4e28-41e8-ac5c-95a2be92f8e2")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
