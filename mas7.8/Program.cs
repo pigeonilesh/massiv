50485ae7fc81228411257d21c74ac827d0fceb99
pigeonIlesh <ilyanikulin0820@gmail.com>
2025-11-19 15:06:57 +0300

    объяснение и начал 7.8

---
+++ mas7.8/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mas7._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Введите количество результатов");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int[] mas = new int[a];
+            Random r = new Random();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = r.Next(0, 101);
+                Console.Write(mas[i]+" ");
+            }
+            Console.WriteLine();
+            double med = 0;
+            if (mas.Length % 2 == 0)
+            {
+                med = (mas[(mas.Length / 2) - 1] + mas[mas.Length / 2]) / 2.0;
+            }
+            else
+                med = mas[mas.Length / 2];
+            Console.WriteLine($"Медиана: {med}");
+            Array.Sort(mas);
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write(mas[i] + " ");
+            }
+            double sum = 0;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                sum += mas[i];
+            }
+            double sred = sum / mas.Length;
+        }
+    }
+}
---
+++ obisnenie/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace obisnenie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] mas =
+            {
+                "ВойнаИМир-Толстой-1869-4,8",
+                "ПреступлениеИНакаание-Достоевский-1866-4,7",
+                "1984-Оруэлл-1949-4,6",
+                "МастерИМаргарита-Булгаков-1967-4,9",
+                "ВластелинКолец-Толкин-1954-4,8",
+                "ГарриПоттер-Роулинг-1997-4,5",
+                "ВишневыйСад-Чехов-1904-4,3",
+                "НоваяКнига-Автор-2020-4,2"
+            };
+            Console.WriteLine("=== Анализ библиотеи ===");
+            Console.WriteLine();
+            Console.WriteLine("Книга с рейтингом больше 4.5");
+            var reit = mas.Where(m => Convert.ToDouble(m.Split('-')[3]) > 4.5);
+            foreach (var a in reit)
+            {
+                Console.WriteLine($"  - {a.Split('-')[0]}");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Книги, которые были выпущенны после 2015 года:");
+            var age = mas.Where(m => Convert.ToInt32(m.Split('-')[2]) > 2015);
+            foreach (var a in age)
+            {
+                Console.WriteLine($"  - {a.Split('-')[0]} - {a.Split('-')[2]}");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Топ три книги");
+            var top3 = mas.OrderByDescending(m => Convert.ToDouble(m.Split('-')[3])).Take(3);
+            foreach (var a in top3)
+            {
+                Console.WriteLine($" -- {a.Split('-')[0]} - {a.Split('-')[3]} --");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Книги, которые начинаются с буквы В:");
+            var b = mas.Where(m => m.StartsWith("В"));
+            foreach (var a in b)
+            {
+                Console.WriteLine($"  - {a.Split('-')[0]}");
+            }    
+        }
+    }
+}
