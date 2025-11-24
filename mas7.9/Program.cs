b18b6f0ce455bd3033ff4d50153b99ba091e5347
pigeonIlesh <ilyanikulin0820@gmail.com>
2025-11-24 22:26:47 +0300

    7.9

---
+++ mas7.9/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mas7._9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Табель оценок 9Б (Рус.яз, Ин.яз, Мат, Инф, Физ)");
+            string[] mas =
+            {
+                "Иванов-5-5-5-5-5",
+                "Петрова-4-5-4-5-4",
+                "Сидоров-5-4-5-4-5",
+                "Козлова-4-4-4-4-3",
+                "Николаев-3-4-3-5-4",
+                "Фёдоров-5-5-3-4-3",
+                "Волкова-4-3-5-3-4",
+                "Орлов-3-5-4-4-2",
+                "Белова-4-3-3-3-3",
+                "Жуков-3-3-4-5-3",
+            };
+            Console.WriteLine();
+            foreach (string tab in mas)
+            {
+                var part = tab.Split('-');
+                Console.WriteLine($"{part[0]}: {part[1]}, {part[2]}, {part[3]}, {part[4]}, {part[5]}");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Введите средний балл");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine($"Ученики со средним баллом выше {a}:");
+            foreach (string tab in mas)
+            {
+                var part = tab.Split('-');
+                string name = part[0];
+                double sum = 0;
+                for (int i = 1; i < part.Length; i++)
+                {
+                    sum += Convert.ToDouble(part[i]);
+                }
+                double sred = sum / (part.Length - 1);
+                if (sred > a)
+                {
+                    Console.WriteLine($"{name}: {sred:f1}");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Успеваимость по предметам:");
+            string[] sub = {"Рус.яз", "Ин.яз", "Мат", "Инф", "Физ"};
+            double[] best = new double[sub.Length];
+            for (int i = 0; i < sub.Length; i++)
+            {
+                double summ = 0;
+                foreach (string tab in mas)
+                {
+                    var part = tab.Split('-');
+                    summ += Convert.ToDouble(part[i + 1]);
+                }
+                best[i] = summ / mas.Length;
+                Console.WriteLine($"{sub[i]}: {best[i]}");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Предмет с наивысшей успеваимостью:");
+            double max = 0;
+            string bsub = "";
+            for (int i = 0; i < best.Length; i++)
+            {
+                if (best[i] > max)
+                {
+                    max = best[i];
+                    bsub = sub[i];
+                }
+            }
+            Console.WriteLine($"{bsub}: {max}");
+        }
+    }
+}
